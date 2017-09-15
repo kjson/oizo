62c34accf3564320159c407848f1c13878fdf576
Kevin Johnson <kevinstuartjohnson@gmail.com>
2017-09-15 04:14:36 -0400

    Handling errors using Results

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-use std::io::Write;
+use std::io::{Result, Write};
 use std::thread;
 use std::net::{Ipv4Addr, TcpListener, TcpStream};
 
 
 
-fn handle_client(mut stream: TcpStream) {
-    stream.write(b"Hello World\r\n").unwrap();
+fn handle_client(mut stream: TcpStream) -> Result<()> {
+    stream.write(b"Hello World\r\n")?;
+    Ok(())
 }
 
 
 fn main() {
     let host = Ipv4Addr::new(127, 0, 0, 1);
     let port = 9000;
-    let listener = TcpListener::bind((localhost, port)).unwrap();
+    let listener = TcpListener::bind((host, port)).unwrap();
 
     for stream in listener.incoming() {
         match stream {

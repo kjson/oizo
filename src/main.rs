39e2d63903f3c821c98d89ad354a7db2f88d9681
Kevin Johnson <kevinstuartjohnson@gmail.com>
2017-09-15 04:04:26 -0400

    Restructuring functions

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 use std::io::Write;
 use std::thread;
-use std::net::{TcpListener};
+use std::net::{Ipv4Addr, TcpListener, TcpStream};
+
+
+
+fn handle_client(mut stream: TcpStream) {
+    stream.write(b"Hello World\r\n").unwrap();
+}
 
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:9123").unwrap();
+    let host = Ipv4Addr::new(127, 0, 0, 1);
+    let port = 9000;
+    let listener = TcpListener::bind((localhost, port)).unwrap();
+
     for stream in listener.incoming() {
-        thread::spawn(|| {
-            let mut stream = stream.unwrap();
-            stream.write(b"Hello World\r\n").unwrap();
-        });
+        match stream {
+            Err(e) => {
+                println!("Failed: {}", e)
+            },
+            Ok(stream) => {
+                thread::spawn(move || { handle_client(stream) });
+            }
+        }
     }
 }
